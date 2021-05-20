9e04ea19e2e584a55a7a284c78159b96adb8a45e
Rahul Dubey <rahuld7219@gmail.com>
2021-05-20 18:20:09 +0530

    Create Seeds to populate the database with some initial campgrounds data

---
+++ seeds/index.js
@@ -0,0 +1,39 @@
+// run this file to seed the database with some initial campgrounds
+
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const { descriptors, places } = require('./seedHelpers');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Database connection error: "));
+db.once("open", () => {
+    console.log("Database connected!!");
+});
+
+
+// returns a random element from the given array
+const pickRandom = (array) => array[Math.floor(Math.random() * array.length)];
+
+// 1st deletes the collection data then insert 50 Campground documents with random title and random location
+const seedDB = async () => {
+    await Campground.deleteMany({});  //deletes everything in the 'campgrounds' collection
+    for (let i = 0; i < 50; i++) {
+        const rand1000 = Math.floor(Math.random() * 1000); // generates a random integer in [0, 999]
+        const camp = new Campground({
+            title: `${pickRandom(descriptors)} ${pickRandom(places)}`,
+            location: `${cities[rand1000].city}, ${cities[rand1000].state}`
+        });
+        await camp.save();
+    }
+};
+
+seedDB().then(() => {               // as seedDB is async function it returns promise
+    mongoose.connection.close();
+});
