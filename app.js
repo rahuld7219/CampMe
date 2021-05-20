d6f7851a8970f2e929856bbb28de7a3b18dfb411
Rahul Dubey <rahuld7219@gmail.com>
2021-05-20 15:02:10 +0530

    Setup initial code

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(8080, () => {
+    console.log("SERVING ON PORT 8080!!!");
+});
