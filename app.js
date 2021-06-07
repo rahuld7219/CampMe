03ccd99289fece186987d90d8fed54984178688c
Rahul Dubey <rahuld7219@gmail.com>
2021-06-07 15:13:35 +0530

    Catch and Handle some async Errors with default Express Error Handler

--- app.js
+++ app.js
@@ -42,10 +42,14 @@ app.get('/campgrounds/new', (req, res) =>{
 });
 
 // Create route, to handle the submitted form data by adding it to the campgrounds collection in database (new->create)
-app.post('/campgrounds', async (req, res) =>{
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
+app.post('/campgrounds', async (req, res, next) =>{
+    try {
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`);
+    } catch (e) {
+        next(e);
+    }
 });
 
 // show route, to show details of a particular campground
@@ -73,6 +77,10 @@ app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+});
+
+app.use((err, req, res, next) => {
+    res.send("Something Went Wrong!");
 });
 
 app.listen(8080, () => {
